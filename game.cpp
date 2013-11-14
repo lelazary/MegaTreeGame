8a9a281b1a247ca58f4c23e0ca3c040d150170a7
Lior Elazary <lelazary@yahoo.com>
2013-11-14 12:09:46 -0800

    Got rgba to work

--- game.cpp
+++ game.cpp
@@ -15,127 +15,128 @@
 #include <stdio.h>
 
 
-struct Geometry
+struct PixelString
 {
   std::vector<CvPoint> points;
   std::vector<CvScalar> colors;
 
-  Geometry(std::vector<CvPoint> p) :
+  PixelString(std::vector<CvPoint> p) :
     points(p)
   {
     for(int i=0; i<points.size(); i++)
-			colors.push_back(CV_RGB(255,0,0));
+      colors.push_back(CV_RGB(255,0,0));
   }
 
-  Geometry(CvPoint p1, CvPoint p2)
+  PixelString(CvPoint p1, CvPoint p2)
   {
-		int dx = p2.x-p1.x;
-		int dy = p2.y-p1.y;
-		double len;
+    int dx = p2.x-p1.x;
+    int dy = p2.y-p1.y;
+    double len;
 
-		if(abs(dx) > abs(dy))
-			len = dx;
-		else
-			len = dy;
+    if(abs(dx) > abs(dy))
+      len = dx;
+    else
+      len = dy;
 
-		double xIncr = dx/len;
-		double yIncr = dy/len;
+    double xIncr = dx/len;
+    double yIncr = dy/len;
 
-		//cvDrawCircle(img, cvPoint(p1.x,p1.y), 1, color,1);
-		//cvSet2D(img, p1.y,p1.x, color);
-		double x=p1.x;
-		double y=p1.y;
-		int step = 2; //floor((len+1)/150);
+    double x=p1.x;
+    double y=p1.y;
+    int step = round((len+1)/150);
 
-		points.push_back(cvPoint(x, y));
-		colors.push_back(CV_RGB(0,0,0));
-		for(int i=1;i<len;i++)
-		{
-			x=x+xIncr;
-			y=y+yIncr;
-			if (!(i%step))
-			{
-				points.push_back(cvPoint(x, y));
-				colors.push_back(CV_RGB(0,0,0));
-			}
-				//cvSet2D(img, y,x, color);
-			//cvDrawCircle(img, cvPoint(x,y), 1, color,1);
-		}
+    points.push_back(cvPoint(x, y));
+    colors.push_back(CV_RGB(0,0,0));
+    for(int i=1;i<len;i++)
+    {
+      x=x+xIncr;
+      y=y+yIncr;
+      if (!(i%step))
+      {
+        points.push_back(cvPoint(x, y));
+        colors.push_back(CV_RGB(0,0,0));
+      }
+    }
   }
 
-	void setColor(int i, CvScalar c) {
-		if (i > 0 && i*3+2 < colors.size())
-		{
-			colors[i*3+0] = c;
-			colors[i*3+1] = c;
-			colors[i*3+2] = c;
-		}
-	}
+  void setColor(int i, CvScalar c) {
+    if (i >= 0 && i*3+2 < colors.size())
+    {
+      //Out string has 3 pixels per index
+      colors[i*3+0] = c;
+      colors[i*3+1] = c;
+      colors[i*3+2] = c;
+    }
+  }
+
+  void setColor(CvScalar c) {
+    for(int i=0; i<colors.size(); i++)
+      colors[i] = c;
+  }
 
   void draw(IplImage* img)
   {
     for(int i=0; i<points.size(); i++)
     {
-			//cvDrawCircle(img, points[i], 1, colors[i]);
-			cvSet2D(img, points[i].y,points[i].x, colors[i]);
+      //cvDrawCircle(img, points[i], 1, colors[i]);
+      cvSet2D(img, points[i].y,points[i].x, colors[i]);
     }
   }
 };
 
-
-void drawLine(IplImage *img, CvPoint p1, CvPoint p2, CvScalar color)
+class MegaTree
 {
-	int dx = p2.x-p1.x;
-	int dy = p2.y-p1.y;
-	double len;
 
-	if(abs(dx) > abs(dy))
-		len = dx;
-	else
-		len = dy;
+public:
+  MegaTree(CvPoint pt, CvPoint size, int numStrings)
+  {
+    for(int i=-numStrings/2; i<numStrings/2; i++)
+      strings.push_back(PixelString(cvPoint((pt.x+1)+(i*2),pt.y),
+                                    cvPoint((pt.x+5)+(i*10),pt.y+size.y)));
+  }
 
-	double xIncr = dx/len;
-	double yIncr = dy/len;
+  void draw(IplImage* img)
+  {
+    for(int i=0; i<strings.size(); i++)
+      strings[i].draw(img);
+  }
 
-	//cvDrawCircle(img, cvPoint(p1.x,p1.y), 1, color,1);
-	cvSet2D(img, p1.y,p1.x, color);
-	double x=p1.x;
-	double y=p1.y;
-	int step = floor((len+1)/150);
-	for(int i=1;i<len;i++)
-	{
-		x=x+xIncr;
-		y=y+yIncr;
-		if (!(i%step))
-			cvSet2D(img, y,x, color);
-			//cvDrawCircle(img, cvPoint(x,y), 1, color,1);
-	}
-}
-	
+  void setImage(cv::Mat& img, int offsetX = 0, int offsetY=0)
+  {
+    unsigned char *imgPtr = (unsigned char*)(img.data);
+    for(int x = 0;x < img.cols ;x++){
+      for(int y = 0;y < img.rows ;y++){
+        cv::Vec4b ptr = img.at<cv::Vec4b>(y,x);
+        if (ptr[3] > 0) //Alpha is either on or off
+          setPixel(x+offsetX, y+offsetY, CV_RGB(ptr[2], ptr[1], ptr[0]));
+      }
+    }
+  }
 
+  void setColor(CvScalar c)
+  {
+    for(int i=0; i<strings.size(); i++)
+      strings[i].setColor(c);
+  }
 
-//std::map<int, Object> objects;
+  void setPixel(int x, int y, CvScalar c)
+  {
+    if (x >= 0 && x < strings.size()) 
+      strings[x].setColor(y, c);
+  }
+    
+
+private:
+
+  std::vector<PixelString> strings;
+
+};
 
 int main(int argc, char *argv[])
 {
   char* filename = argv[1]; 
-  
-	std::vector<Geometry> megaTree;
 
-	megaTree.push_back(Geometry(cvPoint(555-11,355), cvPoint(555-55,650)));
-	megaTree.push_back(Geometry(cvPoint(555-9,355), cvPoint(555-45,650)));
-	megaTree.push_back(Geometry(cvPoint(555-7,355), cvPoint(555-35,650)));
-	megaTree.push_back(Geometry(cvPoint(555-5,355), cvPoint(555-25,650)));
-	megaTree.push_back(Geometry(cvPoint(555-3,355), cvPoint(555-15,650)));
-	megaTree.push_back(Geometry(cvPoint(555-1,355), cvPoint(555-5,650)));
-
-	megaTree.push_back(Geometry(cvPoint(555+1,355), cvPoint(555+5,650)));
-	megaTree.push_back(Geometry(cvPoint(555+3,355), cvPoint(555+15,650)));
-	megaTree.push_back(Geometry(cvPoint(555+5,355), cvPoint(555+25,650)));
-	megaTree.push_back(Geometry(cvPoint(555+7,355), cvPoint(555+35,650)));
-	megaTree.push_back(Geometry(cvPoint(555+9,355), cvPoint(555+45,650)));
-	megaTree.push_back(Geometry(cvPoint(555+11,355),cvPoint(555+55,650)));
-
+  MegaTree megaTree(cvPoint(555,355), cvPoint(0,295), 12);
   IplImage *image;
   if( (image = cvLoadImage( filename, 1)) == 0 )
     return -1;
@@ -143,44 +144,50 @@ int main(int argc, char *argv[])
   cvNamedWindow("Test", 1);
   cvMoveWindow("Test", 0, 0);
 
-	int idx = 0;
+  cv::Mat sprite = cv::imread(argv[2], -1); //Load RGBA png image
+
+  megaTree.setColor(CV_RGB(0,0,255));
+
+  int offset = 0;
+  
   while(1)
   {
 
     IplImage *tmpImg = cvCreateImage( cvSize(image->width, image->height), image->depth, image->nChannels);
     cvCopy(image, tmpImg, NULL);
     //Draw the objects
-		for(int i=0; i<megaTree.size(); i++)
-			megaTree[i].draw(tmpImg);
+    megaTree.draw(tmpImg);
 
     cvShowImage("Test", tmpImg);
     int key = cvWaitKey(10);
     cvReleaseImage(&tmpImg);
+  
+    if (key != -1)
+    {
+      megaTree.setColor(CV_RGB(0,0,255));
+      switch (key)
+      {
+        case 49:
+          offset++;
+          if (offset > 50)
+            offset = 0;
+        
+          break;
+        case 50:
+          break;
+      }
+      printf("Offset %i\n", offset);
+      //megaTree.setPixel(0, offset, CV_RGB(255,0,255));
+      megaTree.setImage(sprite, 0, offset);
+    }
 
-		
-
-		if (key != -1)
-		{
-			printf("Key %i\n", key);
-			switch (key)
-			{
-				case 49:
-					idx++;
-		//			megaTree[5].setColor(idx++, CV_RGB(255,0,0));
-					break;
-
-				case 50:
-					break;
-			}
-		}
-					
   }
 
   /* close device (this not explicitly needed in most implementations) */
 
   cvReleaseImage(&image);
   cvDestroyWindow("Test");
-    
+
   return 0;
 }
 
